966887741bf9f1c76743010ba8d0b563efe79938
RichmondD <45608505+RichmondD@users.noreply.github.com>
2019-07-09 20:45:44 -0700

    eeh

--- Lost Halls Map Reading Practice/src/model/Window.java
+++ Lost Halls Map Reading Practice/src/model/Window.java
@@ -13,50 +13,41 @@ public class Window implements WindowListener {
 	@Override
 	public void windowActivated(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
 
 	@Override
 	public void windowClosed(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
 
 	@Override
 	public void windowClosing(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
 
 	@Override
 	public void windowIconified(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
 
 	@Override
 	public void windowOpened(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
 	
 	public void windowStateChanged(WindowEvent arg0) {
 		Paint.repaint();
-
 	}
-
 }
 
 //Created by Nacnudd and Goalaso from the Pub Halls Discord server.
